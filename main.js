cba3aea4a90c259b6b8b0cd5253f4a5ce631e6b2
Angelo Rodrigues <angelorodrigues318@gmail.com>
2022-08-07 18:39:30 -0300

    Corrections

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
+var lifes = 1;
+
 function adjustScreenSize() {
 
-    if(document.getElementById('inset')) {
-        document.getElementById('inset').remove();
-    }
-
+    
     var height = window.innerHeight;
     var width = window.innerWidth;
 
-    console.log(width, height);
-
-    var positionX = Math.floor(Math.random() * width) - 90;
-    var positionY = Math.floor(Math.random() * height) - 90;
-
-    positionX = positionX < 0 ? 0 : positionX;
-    positionY = positionY < 0 ? 0 : positionY;
-
-    console.log(positionX,positionY);
 
     function randomPosition() {
 
+
+        if(document.getElementById('inset')) {
+            document.getElementById('inset').remove();
+            if(lifes > 3) {
+                alert('Interromper o jogo (Game Over)')
+            } else {
+                document.getElementById('v' + lifes).src = "images/coracao_vazio.png";
+    
+                lifes++;
+            }
+        } 
+
+        console.log(lifes);
+
+        var positionX = Math.floor(Math.random() * width) - 90;
+        var positionY = Math.floor(Math.random() * height) - 90;
+    
+        positionX = positionX < 0 ? 0 : positionX;
+        positionY = positionY < 0 ? 0 : positionY;
+    
         //create the html element
         var inset = document.createElement('img');
         inset.src = 'images/mosca.png';
@@ -27,11 +37,11 @@ function adjustScreenSize() {
         inset.style.top = positionY + 'px';
         inset.style.position = 'absolute';
         inset.id = 'inset';
+        inset.onclick = function() {
+            this.remove();
+        }
     
         document.body.appendChild(inset);
-
-
-        
     }
 
     randomPosition();
@@ -63,6 +73,6 @@ function adjustScreenSize() {
 
 setInterval(function(){
     adjustScreenSize();
-}, 1000)
+}, 2000 )
 
 
